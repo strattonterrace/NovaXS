2b941f9bdcfd1b8aecf6e7dfabdc90739b4cf42f
Laurynas Juzva <laurynas.juzva@gmail.com>
2022-07-24 15:45:45 -0700

    revese

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -12,9 +12,9 @@ const Footer = () => (
           <div className="col-sm-6 col-md-3 item">
                   <h3>Contact Info</h3>
                   <ul>
-                      <li><a href="mailto:jdresel@calmedhawaii.com" target="_blank" rel="noopener noreferrer" >jdresel@calmedhawaii.com</a></li>
-                      <li><a href="facetime:8086919973" target="_blank" rel="noopener noreferrer" >(808) 691-9973 </a></li>
-                      <li><a href="https://goo.gl/maps/2umcZiXxK49MiDqp9" target="_blank" rel="noopener noreferrer" >3049 Ualena Street Unit 101, First Floor Honolulu, HI 96819-1942 </a></li>
+                      <li><a href="mailto:info@novaxs.co" target="_blank" rel="noopener noreferrer" >info@novaxs.co</a></li>
+                      <li><a href="facetime:19493837619" target="_blank" rel="noopener noreferrer" >(949) 383-7619</a></li>
+                      <li><a href="https://goo.gl/maps/oVgYYD13gh37Uavh7" target="_blank" rel="noopener noreferrer" >8 Panorama Trabuco Canyon, CA</a></li>
                   </ul>
               </div>
               <div className="col-sm-6 col-md-3 item">
@@ -28,20 +28,19 @@ const Footer = () => (
               </div>
           
               <div className="col-md-6 item text">
-                  <h3>Cal Med Hawaii</h3>
+                  <h3>NovaXS Biotech</h3>
                   <p>
-                  Cal Med Hawaii is a proud provider of medical supplies to the local people of Hawaii. CalMed Hawaii is dedicated to providing orthopedic, pulmonary, 
-                  mobility and women’s health products for over 20 years. We are a network provider for a majority of Hawaii’s health insurance companies.
+                  NovaXS is a therapeutic startup focused on improving treatment efficacy through an advanced drug delivery platform and innovative CRISPR gene therapy nanoparticle delivery. 
                   </p>
               </div>
               <div className="col item social">
-                <a href="https://www.facebook.com/CalMed-Hawaii-277028104107983/" target="_blank" rel="noopener noreferrer" >  <i className="fab fa-facebook-f "></i></a>
-                <a href="https://twitter.com/calmedhawaii" target="_blank" rel="noopener noreferrer" ><i className="fab fa-twitter "></i></a>
-                <a href="https://www.instagram.com/accounts/login/?next=/calmedhawaii/" target="_blank" rel="noopener noreferrer" ><i className="fab fa-instagram " ></i></a>
-                <a href="https://www.pinterest.com/Calmedhawaii/" target="_blank" rel="noopener noreferrer" ><i className="fab fa-pinterest "></i></a>
+                <a href="https://www.facebook.com/Nova-XS-Biotech-260711482451882/" target="_blank" rel="noopener noreferrer" >  <i className="fab fa-facebook-f "></i></a>
+                <a href="https://www.linkedin.com/company/novaxs-biotech-corp/" target="_blank" rel="noopener noreferrer" ><i className="fab fa-linkedin "></i></a>
+                <a href="mailto:info@novaxs.co" target="_blank" rel="noopener noreferrer" ><i className="fa fa-envelope " ></i></a>
+                <a href="https://goo.gl/maps/kbgx1STFFrQ5FTzh7" target="_blank" rel="noopener noreferrer" ><i className="fab fa-google "></i></a>
                 </div>
           </div>
-          <p className="copyright">Cal Med Hawaii LLC © 2022</p>
+          <p className="copyright">NovaXS Biotech LLC © 2022</p>
       </div>
   </footer>
 </div>
--- src/Components/HomeVideo/HomeVideo.js
+++ src/Components/HomeVideo/HomeVideo.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import './HomeVideo.css'
 import Typewriter from 'typewriter-effect'
-import homeBackground from '../../Assets/videos/bg-home1.jpg'
+import videoBg from '../../Assets/videos/videoBg1.mp4'
 
 const HomeVideo = () => {
   return (
     <div className='main'>
         <div className="overlay"></div>
-        <img src={homeBackground} />
+        <video src={videoBg} autoPlay loop muted type='video/mp4' />
         <div className="content">
         <h1 className="Content__Home__h1">Enabling Greater <br/>Access To
-            <span className="emphasis-1"> Life Improving <br/>  Solutions</span> 
+            <span className="emphasis-1"> Life Improving  Pharmaceuticals</span> 
           </h1>
         <Typewriter
         onInit={(typewriter) => {
@@ -18,7 +18,7 @@ const HomeVideo = () => {
             .typeString('<span style="color: #FAFAFA;"></span>')
             .pauseFor(500)
             .deleteAll()
-            .typeString('<span style="color: #FAFAFA;"> Cal Med Hawaii</span>')
+            .typeString('<span style="color: #FAFAFA;"> NovaXs Biotech </span>')
             .start()
         }}
       />
